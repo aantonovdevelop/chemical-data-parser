8f95b7296b44a737ed6dcb42c85c1696be04ab2e
Artem Antonov <aantonov.develop@gmail.com>
2019-02-09 20:25:30 +0300

    Add user interface and export

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import re
 import os
 import sys
-import statistics
 import mainwindow
-import numpy as np
-import matplotlib.pyplot as plt
+import chemical
 
 from PyQt5 import QtWidgets
-from scipy import interpolate
 
 
 class Application(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
@@ -15,11 +12,63 @@ class Application(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
 
+        self.directory = None
+        self.parse_objects = {}
+
         self.btnOpenDirectory.clicked.connect(self.browse_folder)
+        self.btnExport.clicked.connect(self.export)
+        self.lstFiles.itemClicked.connect(self.preview)
 
     def browse_folder(self):
+        self.lstFiles.clear()
+
         # noinspection PyCallByClass
-        directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Chose directory")
+        self.directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Chose directory")
+
+        if not self.directory:
+            return
+
+        files = [file for file in os.listdir(self.directory) if os.path.isfile(self.directory + "/" + file)]
+
+        for file in files:
+            data = []
+            with open(self.directory + "/" + file, "r") as data_file:
+                for line in data_file:
+                    data.append(list(map(float, re.sub(" +", " ", line).split(" ")[2:])))
+            self.parse_objects[file] = chemical.ChemicalDataParser(data)
+
+            self.lstFiles.addItem(file)
+
+        for parser in self.parse_objects.values():
+            print(parser.difference)
+            print(parser.normalized_data)
+
+        self.btnExport.setEnabled(True)
+
+    def preview(self, file_name_item):
+        parser = self.parse_objects[file_name_item.text()]
+        preview_string = "Difference: " + str(parser.difference) + "\n"
+
+        for point in parser.normalized_data:
+            preview_string += str(point[0]) + "\t" + str(point[1]) + "\n"
+
+        self.txtResult.setText(preview_string)
+
+    def export(self):
+        # noinspection PyCallByClass
+        export_directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Chose directory")
+
+        for file_name in self.parse_objects.keys():
+            parser = self.parse_objects[file_name]
+
+            with open(export_directory + "/" + "differences.txt", "a+") as file:
+                file.write(file_name + " " + str(parser.difference) + "\n")
+
+            with open(export_directory + "/" + file_name + ".dat", "w") as file:
+                for point in parser.normalized_data:
+                    file.write(str(point[0]) + " " + str(point[1]) + "\n")
+
+        QtWidgets.QMessageBox.information(self, "Export", "Data exported")
 
 
 def main():
@@ -30,70 +79,3 @@ def main():
 
 
 main()
-
-
-def normalize(y, min_y, max_y):
-    return (2*(y - min_y) / (max_y - min_y)) - 1
-
-
-data = []
-
-with open("data/c13", "r") as data_file:
-    for line in data_file:
-        data.append(list(map(float, re.sub(" +", " ", line).split(" ")[2:])))
-
-data_x = list(map(lambda v: v[0], data))
-data_y = list(map(lambda v: v[1], data))
-
-min_point = min(data, key=lambda v: v[1])
-max_point = max(data, key=lambda v: v[1])
-
-normalized_y = [normalize(y, min_point[1], max_point[1]) for y in data_y]
-
-median = statistics.median(data_y)
-median_normalized = statistics.median(normalized_y)
-
-data_normalized = [[data_x[i], normalized_y[i]] for i in range(len(normalized_y))]
-
-median_crosses = []
-for i in range(len(data_normalized) - 1):
-    if data_normalized[i][1] > 0.01 and -0.01 > data_normalized[i + 1][1]:
-        median_crosses.append(data_normalized[i])
-        median_crosses.append(data_normalized[i + 1])
-
-# [plt.scatter(point[0], point[1]) for point in median_crosses]
-
-f = interpolate.interp1d(data_x, normalized_y)
-
-interpolated_data = [[x, f(x).tolist()] for x in data_x]
-interpolated_x = list(map(lambda v: v[0], interpolated_data))
-interpolated_y = list(map(lambda v: v[1], interpolated_data))
-
-closest_to_median = []
-for i in range(0, len(median_crosses), 2):
-    points = []
-    for x in np.arange(median_crosses[i][0], median_crosses[i + 1][0], 0.001):
-        points.append([x, abs(f(x).tolist())])
-    closest_to_median.append(min(points, key=lambda v: v[1]))
-
-[plt.scatter(point[0], point[1]) for point in closest_to_median]
-
-x_difference = 0
-if len(closest_to_median) > 1:
-    x_difference = closest_to_median[1][0] - closest_to_median[0][0]
-
-print("Difference: ", x_difference)
-
-# plt.plot(interpolated_x, interpolated_y)
-plt.plot()
-
-plt.plot(data_x, data_y, alpha=0.3)
-plt.plot(data_x, [median] * len(data_x), alpha=0.3)
-plt.plot(data_x, [median_normalized] * len(data_x))
-plt.plot(data_x, normalized_y)
-
-# plt.scatter(min_point[0], min_point[1])
-# plt.scatter(max_point[0], max_point[1])
-
-plt.show()
-
